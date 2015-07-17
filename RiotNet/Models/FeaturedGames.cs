f1b4b68ad20c74646f3a5545ce823dac66709f9a
galoompa <pm.brown@yahoo.ca>
2015-07-17 17:49:18 -0400

    Added models for featured games.

---
+++ RiotNet/Models/FeaturedGames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet.Models
+{
+    /// <summary>
+    /// Contains list of featured games.
+    /// </summary>
+    public class FeaturedGames
+    {
+        /// <summary>
+        ///  Gets or sets the suggested interval to wait before requesting featured games again.
+        /// </summary>
+        public long ClientRefreshInterval { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the list of featured games.
+        /// </summary>
+        public List<FeaturedGameInfo> GameList { get; set; }
+    }
+}
---
+++ RiotNet/Models/Participant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiotNet.Models
+{
+    /// <summary>
+    /// Contains information on a game participant (summoner).
+    /// </summary>
+    public class Participant
+    {
+        /// <summary>
+        ///  Gets or sets flag indicating whether or not this participant is a bot.
+        /// </summary>
+        public bool Bot { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the ID of the champion played by this participant.
+        /// </summary>
+        public long ChampionId { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the ID of the profile icon used by this participant.
+        /// </summary>
+        public long ProfileIconId { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the ID of the first summoner spell used by this participant.
+        /// </summary>
+        public long Spell1Id { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the ID of the second summoner spell used by this participant.
+        /// </summary>
+        public long Spell2Id { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the summoner name of this participant.
+        /// </summary>
+        public string SummonerName { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the team ID of this participant.
+        /// </summary>
+        public long TeamId { get; set; }
+    }
+}
