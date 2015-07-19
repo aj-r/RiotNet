515c527321b6841bdb3d6a76c3cdb27f1c1efca1
red3141 <red3141@gmail.com>
2015-07-19 14:36:42 -0400

    Current game API call

---
+++ RiotNet/RiotClientCurrentGame.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using RiotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet
+{
+    public partial class RiotClient
+    {
+        private IRestRequest GetCurrentGameBySummonerId(long summonerId)
+        {
+            var request = Get("/observer-mode/rest/consumer/getSpectatorGameInfo/{platformId}/{summonerId}");
+            request.AddUrlSegment("summonerId", summonerId.ToString());
+            return request;
+        }
+
+        /// <summary>
+        /// Gets information about the current game a summoner is playing.
+        /// </summary>
+        /// <param name="summonerId">The summoner's summoner ID.</param>
+        /// <returns>The current game information.</returns>
+        public CurrentGameInfo CurrentGameBySummonerId(long summonerId)
+        {
+            return Execute<CurrentGameInfo>(GetCurrentGameBySummonerId(summonerId));
+        }
+
+        /// <summary>
+        /// Gets information about the current game a summoner is playing.
+        /// </summary>
+        /// <param name="summonerId">The summoner's summoner ID.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public Task<CurrentGameInfo> CurrentGameBySummonerIdAsync(long summonerId)
+        {
+            return ExecuteTaskAsync<CurrentGameInfo>(GetCurrentGameBySummonerId(summonerId));
+        }
+    }
+}
