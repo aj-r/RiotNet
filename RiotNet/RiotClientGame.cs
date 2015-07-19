edc21d84f7223c1ae07d0bacc51b59f26f1af31a
red3141 <red3141@gmail.com>
2015-07-19 17:26:22 -0400

    Game API call

---
+++ RiotNet/RiotClientGame.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using RiotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet
+{
+    public partial class RiotClient
+    {
+        private IRestRequest GetGamesBySummonerIdRequest(long summonerId)
+        {
+            var request = Get("/api/lol/{region}/v1.3/game/by-summoner/{summonerId}/recent");
+            request.AddUrlSegment("summonerId", summonerId.ToString());
+            return request;
+        }
+
+        /// <summary>
+        /// Gets the recent games for a summoner.
+        /// </summary>
+        /// <param name="summonerId">The summoner's summoner ID.</param>
+        /// <returns>The summoner's recent games.</returns>
+        public RecentGames GetGamesBySummonerId(long summonerId)
+        {
+            return Execute<RecentGames>(GetGamesBySummonerIdRequest(summonerId));
+        }
+
+        /// <summary>
+        /// Gets the recent games for a summoner.
+        /// </summary>
+        /// <param name="summonerId">The summoner's summoner ID.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public Task<RecentGames> GetGamesBySummonerIdAsync(long summonerId)
+        {
+            return ExecuteTaskAsync<RecentGames>(GetGamesBySummonerIdRequest(summonerId));
+        }
+    }
+}
