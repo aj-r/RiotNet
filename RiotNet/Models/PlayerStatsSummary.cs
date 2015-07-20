f7a6fc675e8b5259f4b5b1ef880d9b34efcfb336
galoompa <pm.brown@yahoo.ca>
2015-07-20 18:15:52 -0400

    finish datatypes for stats api, fix some comments

---
+++ RiotNet/Models/PlayerStatsSummary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiotNet.Models
+{
+    /// <summary>
+    /// This object contains player stats summary information.
+    /// </summary>
+    public class PlayerStatsSummary
+    {
+        /// <summary>
+        /// Gets or sets aggregated stats.
+        /// </summary>
+        public AggregatedStats AggregatedStats { get; set; }
+
+        /// <summary>
+        /// Gets or sets number of losses for this queue type. Returned for ranked queue types only.
+        /// </summary>
+        public int Losses { get; set; }
+
+        /// <summary>
+        /// Gets or sets date stats were last modified.
+        /// </summary>
+        public DateTime ModifyDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets player stats summary type.
+        /// </summary>
+        // TODO: we should be able to use the enum GameMode, except that the strings are slightly different (no underscores...)
+        // TODO: create custom converter.
+        public GameMode PlayerStatSummaryType { get; set; }
+
+        /// <summary>
+        /// Gets or sets number of wins for this queue type.
+        /// </summary>
+        public int Wins { get; set; }
+    }
+}
---
+++ RiotNet/Models/PlayerStatsSummaryList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet.Models
+{
+    /// <summary>
+    /// This object contains a collection of player stats summary information. Each one is associated with a different game mode.
+    /// </summary>
+    public class PlayerStatsSummaryList
+    {
+        /// <summary>
+        /// Gets or sets several collections of player stat summaries associated with the summoner.
+        /// </summary>
+        public List<PlayerStatsSummary> PlayerStatSummaries { get; set; }
+
+        /// <summary>
+        /// Gets or sets summoner ID.
+        /// </summary>
+        public long SummonerId { get; set; }
+    }
+}
