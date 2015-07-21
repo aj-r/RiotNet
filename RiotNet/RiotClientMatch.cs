1ce2a1ce90d106b6d343bc5328d3f15d5364d8da
red3141 <red3141@gmail.com>
2015-07-20 23:50:20 -0400

    Match API call

---
+++ RiotNet/RiotClientMatch.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using RiotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotNet
+{
+    public partial class RiotClient
+    {
+        private IRestRequest GetMatchRequest(long matchId)
+        {
+            var request = Get("api/lol/{region}/v2.2/match/{matchId}");
+            request.AddUrlSegment("matchId", matchId.ToString());
+            return request;
+        }
+
+        /// <summary>
+        /// Gets the details of a match.
+        /// </summary>
+        /// <param name="matchId">The ID of the match.</param>
+        /// <returns>The details of the match.</returns>
+        public MatchDetail GetMatch(long matchId)
+        {
+            return Execute<MatchDetail>(GetMatchRequest(matchId));
+        }
+
+        /// <summary>
+        /// Gets the details of a match.
+        /// </summary>
+        /// <param name="matchId">The ID of the match.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public Task<MatchDetail> GetMatchTaskAsync(long matchId)
+        {
+            return ExecuteTaskAsync<MatchDetail>(GetMatchRequest(matchId));
+        }
+    }
+}
