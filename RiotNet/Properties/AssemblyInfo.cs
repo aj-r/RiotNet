69ace503347894d2ffa29142b9752f71737229ee
AJ Richardson <aj.ric.ar+git@gmail.com>
2016-11-12 13:15:31 -0500

    Fix some dependency issues

--- RiotNet/Properties/AssemblyInfo.cs
+++ RiotNet/Properties/AssemblyInfo.cs
@@ -17,10 +17,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c2183835-da98-45e0-818f-eccfc391f73d")]
-
 [assembly: AssemblyVersion("3.0.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
 [assembly: AssemblyInformationalVersion("3.0.0")]
