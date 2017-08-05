c1cc7bac351c4f6b232fa83e36a7409f900fdab0
AJ Richardson <aj.ric.ar+git@gmail.com>
2017-08-05 16:06:14 -0400

    Targeted netstandard 1.5, fixed nuget package properties

--- RiotNet/Properties/AssemblyInfo.cs
+++ RiotNet/Properties/AssemblyInfo.cs
@@ -1,22 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RiotNet")]
-[assembly: AssemblyDescription("A client for the Riot Games API. This client is very flexible and configurable, has synchronous and asynchronous versions of each method, and has models that can easily be cached in a database with tools like Entity Framework.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AJ Richardson")]
-[assembly: AssemblyProduct("RiotNet")]
-[assembly: AssemblyCopyright("Copyright 2016 AJ Richardson")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.3.0")]
-[assembly: AssemblyInformationalVersion("4.3.0")]
