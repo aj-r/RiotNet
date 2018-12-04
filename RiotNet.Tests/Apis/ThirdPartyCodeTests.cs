2f89395ed6ad6b93108d9e8a7a9248a901221eae
dfernandezramos <dfernandez@fluendo.com>
2018-12-04 18:08:54 +0100

    Update Third-party to V4

--- RiotNet.Tests/Apis/ThirdPartyCodeTests.cs
+++ RiotNet.Tests/Apis/ThirdPartyCodeTests.cs
@@ -7,11 +7,22 @@ namespace RiotNet.Tests
     [TestFixture]
     public class ThirdPartyCodeTests : TestBase
     {
+        string encryptedSummonerId;
+
+        [OneTimeSetUp]
+        public async Task TestOneTimeSetUp()
+        {
+            SetRiotClientSettings();
+            IRiotClient client = new RiotClient();
+            var summoner = await client.GetSummonerBySummonerNameAsync("dfernandezramos", PlatformId.EUW1);
+            encryptedSummonerId = summoner.Id;
+        }
+
         [Test]
         public async Task GetThirdPartyCodeAsyncTest()
         {
-            IRiotClient client = RiotClient.ForPlatform(PlatformId.NA1);
-            string code = await client.GetThirdPartyCodeAsync(34172230);
+            IRiotClient client = RiotClient.ForPlatform(PlatformId.EUW1);
+            string code = await client.GetThirdPartyCodeAsync(encryptedSummonerId);
 
             Assert.That(code, Is.Not.Null.And.Not.Empty);
         }
