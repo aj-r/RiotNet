1b5e15be08edc6c38f8a0909090c5a6e28ae48c4
dfernandezramos <dfernandez@fluendo.com>
2018-12-04 17:24:29 +0100

    Update Summoner to V4

--- RiotNet/RiotClientSummoner.cs
+++ RiotNet/RiotClientSummoner.cs
@@ -13,7 +13,7 @@ namespace RiotNet
         /// <param name="platformId">The platform ID of the server to connect to. This should equal one of the <see cref="Models.PlatformId"/> values. If unspecified, the <see cref="PlatformId"/> property will be used.</param>
         /// <param name="token">The cancellation token to cancel the operation.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        Task<Summoner> GetSummonerByAccountIdAsync(long accountId, string platformId = null, CancellationToken token = default(CancellationToken));
+        Task<Summoner> GetSummonerByAccountIdAsync(string accountId, string platformId = null, CancellationToken token = default(CancellationToken));
 
         /// <summary>
         /// Gets the summoner information for the specified summoner name. This method uses the Summoner API.
@@ -31,7 +31,7 @@ namespace RiotNet
         /// <param name="platformId">The platform ID of the server to connect to. This should equal one of the <see cref="Models.PlatformId"/> values. If unspecified, the <see cref="PlatformId"/> property will be used.</param>
         /// <param name="token">The cancellation token to cancel the operation.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        Task<Summoner> GetSummonerBySummonerIdAsync(long summonerId, string platformId = null, CancellationToken token = default(CancellationToken));
+        Task<Summoner> GetSummonerBySummonerIdAsync(string summonerId, string platformId = null, CancellationToken token = default(CancellationToken));
     }
 
     public partial class RiotClient
@@ -45,7 +45,7 @@ namespace RiotNet
         /// <param name="platformId">The platform ID of the server to connect to. This should equal one of the <see cref="Models.PlatformId"/> values. If unspecified, the <see cref="PlatformId"/> property will be used.</param>
         /// <param name="token">The cancellation token to cancel the operation.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        public Task<Summoner> GetSummonerByAccountIdAsync(long accountId, string platformId = null, CancellationToken token = default(CancellationToken))
+        public Task<Summoner> GetSummonerByAccountIdAsync(string accountId, string platformId = null, CancellationToken token = default(CancellationToken))
         {
             return GetAsync<Summoner>($"{summonerBasePath}/summoners/by-account/{accountId}", $"{summonerBasePath}/summoners/by-account/{{accountId}}",
                 platformId, token);
@@ -71,7 +71,7 @@ namespace RiotNet
         /// <param name="platformId">The platform ID of the server to connect to. This should equal one of the <see cref="Models.PlatformId"/> values. If unspecified, the <see cref="PlatformId"/> property will be used.</param>
         /// <param name="token">The cancellation token to cancel the operation.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        public Task<Summoner> GetSummonerBySummonerIdAsync(long summonerId, string platformId = null, CancellationToken token = default(CancellationToken))
+        public Task<Summoner> GetSummonerBySummonerIdAsync(string summonerId, string platformId = null, CancellationToken token = default(CancellationToken))
         {
             return GetAsync<Summoner>($"{summonerBasePath}/summoners/{summonerId}", $"{summonerBasePath}/summoners/{{summonerId}}",
                 platformId, token);
